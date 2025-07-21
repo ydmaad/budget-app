8792feb5131a568c9f07d398d0505921629e4945
Minae <ydmaad@gmail.com>
2025-07-21 23:20:07 +0900

    feat: 생성, 목록 불러오기, 삭제 기능 완료 | 수정기능 추가중

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,107 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, type FormEvent } from "react";
 import "./App.css";
+import type { Transaction } from "./types";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [formData, setFormData] = useState({
+    amount: "",
+    date: "",
+    category: "",
+    type: "expense" as "income" | "expense",
+  });
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editingId, setEditingId] = useState<string | null>(null);
 
+  const addTransaction = (newTransaction: Transaction) => {
+    setTransactions([...transactions, newTransaction]);
+  };
+  console.log(transactions);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Date.now().toString(),
+      amount: Number(formData.amount),
+      date: formData.date,
+      category: formData.category,
+      type: formData.type,
+    };
+
+    addTransaction(newTransaction);
+    setFormData({
+      amount: "",
+      date: "",
+      category: "",
+      type: "expense",
+    });
+  };
+
+  const handleDelete = (id: string) => {
+    setTransactions(transactions.filter((item) => item.id !== id));
+  };
+
+  const handleEdit = (transaction: Transaction) => {
+    setIsEditing(true);
+    setEditingId(transaction.id);
+    setFormData({
+      amount: transaction.amount.toString(),
+      date: transaction.date,
+      category: transaction.category,
+      type: transaction.type,
+    });
+  };
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          value={formData.amount}
+          onChange={(e) => setFormData({ ...formData, amount: e.target.value })}
+          placeholder="금액"
+        />
+        <input
+          type="date"
+          value={formData.date}
+          onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+        />
+        <input
+          type="text"
+          value={formData.category}
+          onChange={(e) =>
+            setFormData({ ...formData, category: e.target.value })
+          }
+          placeholder="카테고리"
+        />
+        <select
+          value={formData.type}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              type: e.target.value as "income" | "expense",
+            })
+          }
+        >
+          <option value="income">수익</option>
+          <option value="expense">지출</option>
+        </select>
+        <button type="submit">추가</button>
+      </form>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {transactions.map((transaction) => (
+          <div key={transaction.id}>
+            <div>
+              <p>{transaction.type === "income" ? "수익" : "지출"}</p>
+              <p>{transaction.date}</p>
+              <p>{transaction.category}</p>
+              <p>{transaction.amount.toLocaleString()}원</p>
+              <button onClick={() => handleEdit(transaction)}>수정</button>
+              <button onClick={() => handleDelete(transaction.id)}>삭제</button>
+            </div>
+          </div>
+        ))}
       </div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
